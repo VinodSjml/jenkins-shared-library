8d279ddb40a499e54cbd9a3efc39680e8d6b180e
vinod sjml <vinsjml@gmail.com>
2024-01-22 23:05:16 +0530

    push

--- vars/python.groovy
+++ vars/python.groovy
@@ -1,7 +1,7 @@
 def lintChecks() {
     sh "echo starting lint checks for ${component}..."
     sh "pip install pylint"
-    sh "pylint .*.py || true"
+    sh "pylint *.py || true"
     sh "echo lint checks for ${component} are completed..!"
 }
 
