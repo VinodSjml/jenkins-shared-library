182837e9814731ff6e3f33fc0fca1090fbb1b590
vinod sjml <vinsjml@gmail.com>
2024-01-23 23:19:08 +0530

    push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,12 +9,21 @@ def lintChecks() {
 def call() {
         pipeline {
         agent any
+        environment {
+            SONAR_URL = "172.31.38.57"
+            SONAR_CRED = credentials('SONAR_CRED')
+        }
         stages {
             stage('lint check'){
                 steps{
                     script{
                         lintChecks()
                     }
+                }
+            }
+            stage('sonar check'){
+                steps{
+                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectkey=${component} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
                 }
             }
             stage('generating artifacts'){
