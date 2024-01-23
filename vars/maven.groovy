64a7f0ec3885ab8f11324871bcd1e55531b25b5b
vinod sjml <vinsjml@gmail.com>
2024-01-24 00:21:33 +0530

    push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -7,6 +7,10 @@ def lintChecks() {
 def call() {
         pipeline {
         agent any
+        environment {
+            SONAR_URL = "172.31.38.57"
+            SONAR_CRED = credentials('SONAR_CRED')
+        }
         stages {
             stage('lint check'){
                 steps{
@@ -18,6 +22,13 @@ def call() {
             stage('performing code compilation'){
                 steps{
                     sh "mvn clean compile"
+                }
+            }
+            stage('sonar checks'){
+                steps{
+                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.java.binaries=target/ -Dsonar.projectKey=${component} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
+                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+                    sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${component}"               
                 }
             }
             stage('generating artifacts'){
