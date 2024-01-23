7a76b7ae878711d01c7614b0e7e73376c4c0985c
vinod sjml <vinsjml@gmail.com>
2024-01-24 00:07:03 +0530

    push

--- vars/python.groovy
+++ vars/python.groovy
@@ -8,6 +8,10 @@ def lintChecks() {
 def call() {
         pipeline {
         agent any
+        environment {
+            SONAR_URL = "172.31.38.57"
+            SONAR_CRED = credentials('SONAR_CRED')
+        }
         stages {
             stage('lint check'){
                 steps{
