07768997b362de0273435a17298eda9396cf32b9
vinod sjml <vinsjml@gmail.com>
2024-01-24 10:03:53 +0530

    push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -26,9 +26,8 @@ def call() {
             }
             stage('sonar checks'){
                 steps{
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.java.binaries=target/ -Dsonar.projectKey=${component} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
-                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
-                    sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${component}"               
+                    env.ARGS="-Dsonar.java.binaries=target/"
+                    common.sonarChecks()          
                 }
             }
             stage('generating artifacts'){
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -23,10 +23,9 @@ def call() {
             }
             stage('sonar check'){
                 steps{
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${component} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
-                    sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
-                    sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${component}"
-                }
+                    env.ARGS="-Dsonar.sources=."
+                    common.sonarChecks()  
+                    }
             }
             stage('generating artifacts'){
                 steps{
