d7dcea495117a4df3f5e5a10d14faa1e809a6972
vinod sjml <vinsjml@gmail.com>
2024-01-31 17:19:35 +0530

    push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -58,7 +58,8 @@ def call() {
                 when {tag ""}
                 steps{
                     script{
-                        def output = sh(returnStdout: true, script: "curl -v -u ${NEXUS_CRED_USR}:${NEXUS_CRED_PSW} -s -X GET 'http://${NEXUS_URL}:8081/service/rest/v1/components?repository=${component}' | jq ".items[].name" | grep catalogue-001 | sed -e 's/\"//g'")
+                        def output = sh(returnStdout: true, script: "curl -v -u ${NEXUS_CRED_USR}:${NEXUS_CRED_PSW} -s -X GET 'http://${NEXUS_URL}:8081/service/rest/v1/components?repository=${component}' | grep -m1 catalogue-001 | sed -e 's/\"//g' -e 's/    name : //g'
+                        | awk -F . '{print $1}'")
                         print output
                     }
                 }
