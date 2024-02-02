b169612eedc73237c870a5e52e39c8400f4e4f4e
vinod sjml <vinsjml@gmail.com>
2024-02-02 17:06:15 +0530

    push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,11 +1,11 @@
 def sonarChecks(){
-    node {
-sh "echo starting sonar checks for ${Component}.."
-// sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ $ARGS -Dsonar.projectKey=${component} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
-// sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
-// sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${component}" 
-sh "echo sonar check for $ARGS ${Component} are completed!"   
-}
+  stage('Sonar Checks'){    
+    sh "echo starting sonar checks for ${Component}.."
+    // sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ $ARGS -Dsonar.projectKey=${component} -Dsonar.login=${SONAR_CRED_USR} -Dsonar.password=${SONAR_CRED_PSW}"
+    // sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+    // sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PSW} ${SONAR_URL} ${component}" 
+    sh "echo sonar check for $ARGS ${Component} are completed!"   
+  }
 }
 
 def lintChecks(){
