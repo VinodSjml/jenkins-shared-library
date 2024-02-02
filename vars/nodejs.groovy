0c9df26a1822b34768faf56ca3b19314be2ea722
vinod sjml <vinsjml@gmail.com>
2024-02-02 16:48:51 +0530

    push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,12 +7,14 @@ def lintChecks() {
 }
 
 def call(){
-    common.lintChecks()
-    env.ARGS="-Dsonar.sources=."
-    common.sonarChecks()
-    common.testCases()
-    env.NEXUS_URL="172.31.25.180"
-    common.artifacts()
+    node{
+        common.lintChecks()
+        env.ARGS="-Dsonar.sources=."
+        common.sonarChecks()
+        common.testCases()
+        env.NEXUS_URL="172.31.25.180"
+        common.artifacts()
+    }
 }
 
 /*
