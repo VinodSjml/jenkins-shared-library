74d706a59d34c8751e132069e8a1ae4d3ffe12d4
vinod sjml <vinsjml@gmail.com>
2024-02-06 11:57:08 +0530

    push

--- vars/python.groovy
+++ vars/python.groovy
@@ -12,6 +12,7 @@ def call(){
         env.ARGS="-Dsonar.sources=."
         common.sonarChecks()
         common.testCases()
+        env.NEXUS_URL="172.31.25.180"
         common.artifacts()
     }
 }
