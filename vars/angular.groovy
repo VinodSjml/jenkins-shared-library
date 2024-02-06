77f0da62246d6c6f9e60f2220ebcce2d2499d0f0
vinod sjml <vinsjml@gmail.com>
2024-02-06 19:15:42 +0530

    push

--- vars/angular.groovy
+++ vars/angular.groovy
@@ -1,10 +1,11 @@
 def call(){
-    node{
-        common.lintChecks()
-        env.ARGS="-Dsonar.sources=."
-        common.sonarChecks()
-        common.testCases()
-        env.NEXUS_URL="172.31.25.180"
-        common.artifacts()
-    }
+ node{
+    git branch: 'main', url: "https://github.com/VinodSjml/${Component}.git"
+    common.lintChecks()
+    env.ARGS="-Dsonar.sources=."
+    common.sonarChecks()
+    common.testCases()
+    env.NEXUS_URL="172.31.25.180"
+    common.artifacts()
+  }
 }
